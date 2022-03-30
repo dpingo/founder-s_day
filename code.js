765cc100b97e9fa125551837debb852cd29f611c
dpingo <95493496+dpingo@users.noreply.github.com>
2022-03-30 22:26:22 +0530

    Add files via upload

---
+++ code.js
@@ -0,0 +1,4 @@
+function change_color(){
+    document.getElementById("intro").style.color="#6d22a3";}
+   // function change_color(){
+      //  document.getElementsByClassName("para").style.color="#6d22a3";}
